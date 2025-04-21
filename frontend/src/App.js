bfc4d57d233afb1f86f2de034f4c368d2f1f563b
rajnikantk0710 <rajnikantk0710@gmail.com>
2025-04-22 00:48:14 +0530

    Add frontend/src/App.js 1745263093581

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Dashboard from './pages/Dashboard';
+import Subscription from './pages/Subscription';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/subscription" element={<Subscription />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
